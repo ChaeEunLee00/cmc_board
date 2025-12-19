7be02eaa261b3ffac51c2dd4b866bdfc42429650
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2025-12-19 17:19:47 +0900

    setting

---
+++ src/test/java/com/cmc/board/CmcBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmcBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
